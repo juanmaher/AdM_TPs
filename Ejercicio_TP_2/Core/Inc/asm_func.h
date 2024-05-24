b1707f121dadd1571202b176a48f86be25c361b6
fedealde <62813604+fedealde@users.noreply.github.com>
2024-05-24 19:52:00 -0300

    TP2 init

---
+++ Ejercicio_TP_2/Core/Inc/asm_func.h
@@ -0,0 +1,15 @@
+/*
+ * asm_func.h
+ *
+ *  Created on: May 16, 2024
+ *      Author: flealu
+ */
+
+#ifndef INC_ASM_FUNC_H_
+#define INC_ASM_FUNC_H_
+
+void asm_pack32to16 (int32_t * vectorIn, int16_t *vectorOut, uint32_t longitud);
+
+
+
+#endif /* INC_ASM_FUNC_H_ */
